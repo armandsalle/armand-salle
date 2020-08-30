e6170093c2df6c81be40671499495806108eba17
Armand Sallé <armand.salle@gmail.com>
2020-08-30 17:43:18 +0200

    Add components to About page

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -3,8 +3,9 @@ import { useInView } from "react-intersection-observer"
 import anime from "animejs"
 import Title from "./title"
 import Img from "gatsby-image"
+import cn from "classname"
 
-const Hero = React.memo(({ title, imagePath }) => {
+const Hero = React.memo(({ title, imagePath, about }) => {
   const [hasDelay, setDelay] = useState(true)
 
   const [imgRef, inView] = useInView({
@@ -34,24 +35,44 @@ const Hero = React.memo(({ title, imagePath }) => {
   return (
     <div className="container mt-3">
       <div className="hero">
-        <div className="img-wrapper" ref={imgRef}>
-          <Img
-            fluid={imagePath.src.childImageSharp.fluid}
-            alt={imagePath.alt}
-          />
+        <div className={cn("img-wrapper", about && "--about")} ref={imgRef}>
+          {!about && (
+            <Img
+              fluid={imagePath.src.childImageSharp.fluid}
+              alt={imagePath.alt}
+            />
+          )}
+          {about && (
+            <>
+              <div className="about-hero-img">
+                <Img
+                  fluid={imagePath.src.childImageSharp.fluid}
+                  alt={imagePath.alt}
+                />
+              </div>
+              <div className="about-hero-img">
+                <Img
+                  fluid={imagePath.src.childImageSharp.fluid}
+                  alt={imagePath.alt}
+                />
+              </div>
+            </>
+          )}
         </div>
-        {title.map((el, i) => (
-          <h2 className={`${i === 1 ? "h2 align-right" : "h2"}`} key={i}>
-            {el.map((w, i) => (
-              <Title
-                word={w.text}
-                key={i}
-                variant={w?.variant}
-                hasDelay={hasDelay}
-              />
-            ))}
-          </h2>
-        ))}
+        <div className={cn(about && "hero-about")}>
+          {title.map((el, i) => (
+            <h2 className={`${i === 1 ? "h2 align-right" : "h2"}`} key={i}>
+              {el.map((w, i) => (
+                <Title
+                  word={w.text}
+                  key={i}
+                  variant={w?.variant}
+                  hasDelay={hasDelay}
+                />
+              ))}
+            </h2>
+          ))}
+        </div>
       </div>
     </div>
   )
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,47 @@
 import React from "react"
 import SEO from "../components/seo"
+import HeroTitle from "../components/heroTitle"
+import Hero from "../components/hero"
+import { graphql } from "gatsby"
 
-const AboutPage = () => (
-  <div>
+const AboutPage = ({
+  data: {
+    title: { title, location, year },
+    hero: { heroTitle, aboutImagePath },
+  },
+}) => (
+  <>
     <SEO title="Home" />
-    <h1>hello</h1>
-  </div>
+    <HeroTitle title={title} infoFirstLine={location} infoSecondLine={year} />
+    <Hero title={heroTitle} imagePath={aboutImagePath} about />
+    <section className="home-about"></section>
+  </>
 )
+
+export const aboutQuery = graphql`
+  query About {
+    title: heroTitleJson {
+      location
+      title
+      year
+    }
+    hero: heroJson {
+      aboutImagePath {
+        src {
+          childImageSharp {
+            fluid(maxWidth: 710, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        alt
+      }
+      heroTitle: title {
+        text
+        variant
+      }
+    }
+  }
+`
 
 export default AboutPage
