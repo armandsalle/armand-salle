ed883acb086b043436f30b9edd995178324681f7
Armand Sallé <armand.salle@gmail.com>
2020-09-06 00:23:07 +0200

    add context provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,6 @@
+import React from "react"
+import { LayoutProvider } from "./src/contexts/layoutContext"
 
+export const wrapRootElement = ({ element }) => {
+  return <LayoutProvider>{element}</LayoutProvider>
+}
