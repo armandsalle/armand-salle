dd4c00dc63e2a596287d97ff6e30af94064eb7ef
Armand Sallé <armand.salle@gmail.com>
2020-09-10 15:49:49 +0200

    create project hero component

---
+++ src/components/projectHero/index.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react"
+import ProjectContent from "../projectContent"
+import { useInView } from "react-intersection-observer"
+import anime from "animejs"
+
+const ProjectHero = ({ topDesc, link, thumbnail }) => {
+  const [thumbRef, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: "0px",
+  })
+
+  useEffect(() => {
+    if (inView) {
+      anime({
+        targets: ".project-hero__thumb",
+        opacity: [0, 1],
+        easing: "easeOutSine",
+        duration: 600,
+        delay: 1000,
+      })
+    }
+  }, [inView])
+
+  return (
+    <section className="project-hero">
+      <div
+        ref={thumbRef}
+        className="project-hero__thumb"
+        style={{ backgroundImage: `url(${thumbnail.publicURL})` }}
+      ></div>
+      <ProjectContent link={link} text={topDesc} />
+    </section>
+  )
+}
+
+export default ProjectHero
