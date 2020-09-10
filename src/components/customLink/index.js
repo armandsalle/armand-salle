86fbc6146c163695c8ae0619ced91659322c1d5d
Armand Sallé <armand.salle@gmail.com>
2020-09-10 15:48:48 +0200

    native link or gatsby link

--- src/components/customLink/index.js
+++ src/components/customLink/index.js
@@ -1,15 +1,34 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const CustomLink = ({ to, text, textLink }) => {
-  return (
-    <Link className="link" to={to}>
+const CustomLink = ({ to, text, textLink, native, blank }) => {
+  const inner = (
+    <>
       <span className="link__text">{text}</span>
       <span className="link__text-link">
         <span className="link__text-link__normal">{textLink}</span>
         <span className="link__text-link__italic">{textLink}</span>
       </span>
-    </Link>
+    </>
+  )
+
+  return (
+    <>
+      {native ? (
+        <a
+          className="link"
+          href={to}
+          target={blank && "_blank"}
+          rel="noreferrer"
+        >
+          {inner}
+        </a>
+      ) : (
+        <Link className="link" to={to}>
+          {inner}
+        </Link>
+      )}
+    </>
   )
 }
 
