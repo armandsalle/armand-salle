b944d2fc1eee9f8099e8699ea8702672e27d816d
Armand Sallé <armand.salle@gmail.com>
2020-09-10 15:51:40 +0200

    add projects components

--- src/templates/project.js
+++ src/templates/project.js
@@ -2,6 +2,10 @@ import React from "react"
 import { graphql } from "gatsby"
 import SEO from "../components/seo"
 import HeroTitle from "../components/heroTitle"
+import ProjectHero from "../components/projectHero"
+import ProjectImages from "../components/projectImages"
+import ProjectContent from "../components/projectContent"
+import NextProject from "../components/nextProject"
 
 const ProjectTemplate = ({
   data: {
@@ -9,11 +13,31 @@ const ProjectTemplate = ({
   },
   pageContext: { slug },
 }) => {
-  const { title, year, type } = projects.find(p => p.slug === slug)
+  const {
+    title,
+    year,
+    type,
+    thumbnail,
+    link,
+    topDesc,
+    bottomDesc,
+    images,
+    id,
+  } = projects.find(p => p.slug === slug)
+
+  const projectIds = projects.map(project => project.id)
+  const maxId = Math.max(...projectIds)
+  const nextProjectId = id + 1 > maxId ? 1 : id + 1
+  const nextProject = projects.find(p => p.id === nextProjectId)
+
   return (
     <>
       <SEO title={title} />
       <HeroTitle title={title} infoFirstLine={type} infoSecondLine={year} />
+      <ProjectHero thumbnail={thumbnail} link={link} topDesc={topDesc} />
+      <ProjectImages images={images} />
+      <ProjectContent link={link} text={bottomDesc} />
+      <NextProject project={nextProject} />
     </>
   )
 }
@@ -22,10 +46,30 @@ export const projectQuery = graphql`
   query Project {
     projectsJson {
       projects {
+        id
         title
         slug
         year
         type
+        thumbnail {
+          publicURL
+        }
+        link
+        topDesc
+        bottomDesc
+        images {
+          id
+          src {
+            childImageSharp {
+              fluid(maxWidth: 710, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          col
+          offset
+          alt
+        }
       }
     }
   }
