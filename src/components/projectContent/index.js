abec23ecc08e1c355adb456b84cc0c1860c08edf
Armand Sallé <armand.salle@gmail.com>
2020-09-10 15:49:26 +0200

    create project content component

---
+++ src/components/projectContent/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import CustomLink from "../customLink"
+import Text from "../text"
+
+const ProjectContent = ({ link, text }) => {
+  return (
+    <div className="project-content">
+      <CustomLink to={link} text="Launch" textLink="site" native blank />
+      <Text as="p" className="project-content__text">
+        {text}
+      </Text>
+    </div>
+  )
+}
+
+export default ProjectContent
