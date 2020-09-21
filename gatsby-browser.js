f2d274f127ac322bb8c68d183c308bc9ac339d67
Armand Sallé <armand.salle@gmail.com>
2020-09-21 12:22:10 +0200

    comments

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,20 @@
 import React from "react"
 import { LayoutProvider } from "./src/contexts/layoutContext"
 import { ProjectHoverProvider } from "./src/contexts/projectHoverContext"
+// import imagesLoaded from "imagesloaded"
+
+// window.addEventListener("DOMContentLoaded", () =>
+//   console.log("DOMContentLoaded")
+// )
+// window.addEventListener("load", () => console.log("load"))
+
+// document.fonts.ready.then(function () {
+//   console.log("Fonts ready")
+// })
+
+// imagesLoaded(document.querySelector("body"), () => {
+//   console.log("all images are loaded")
+// })
 
 export const wrapRootElement = ({ element }) => {
   return (
