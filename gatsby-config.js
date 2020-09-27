18168010f2424e44e0d3cb44f3ab2ff469d4716a
Armand Sallé <armand.salle@gmail.com>
2020-09-27 15:13:12 +0200

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,10 +44,7 @@ module.exports = {
         name: `Armand Sallé - Creative developer`,
         short_name: `Armand Sallé`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.svg`,
       },
     },
   ],
