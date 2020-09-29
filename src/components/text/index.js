a9799edeb08906feee3b9961cdb429d955cad096
Armand Sallé <armand.salle@gmail.com>
2020-09-29 12:14:27 +0200

    memoised components

--- src/components/text/index.js
+++ src/components/text/index.js
@@ -3,68 +3,81 @@ import { useInView } from "react-intersection-observer"
 import anime from "animejs"
 import Line from "./line"
 
-const Text = ({ children, col, className, splitAndAnime, as, love }) => {
-  const inner = children.split("<br />").map((text, i) =>
-    love && i === 0 ? (
-      <Line key={i} className="color-primary">
-        {text}
-      </Line>
-    ) : (
-      <Line key={i}>{text}</Line>
+const Text = React.memo(
+  ({ children, col, className, splitAndAnime, as, love }) => {
+    const inner = useRef(
+      children.split("<br />").map((text, i) =>
+        love && i === 0 ? (
+          <Line key={i} className="color-primary">
+            {text}
+          </Line>
+        ) : (
+          <Line key={i}>{text}</Line>
+        )
+      )
     )
-  )
 
-  const textRef = useRef(null)
-  const [textViewRef, inView] = useInView({
-    triggerOnce: true,
-    rootMargin: "0px",
-    threshold: 0.2,
-  })
+    const textRef = useRef(null)
+    const [textViewRef, inView] = useInView({
+      triggerOnce: true,
+      rootMargin: "0px",
+      threshold: 0.2,
+    })
 
-  const setRefs = useCallback(
-    node => {
-      textRef.current = node
-      textViewRef(node)
-    },
-    [textViewRef]
-  )
+    const setRefs = useCallback(
+      node => {
+        textRef.current = node
+        textViewRef(node)
+      },
+      [textViewRef]
+    )
 
-  useEffect(() => {
-    if (inView) {
-      // Text line animation
-      anime({
-        targets: textRef.current.querySelectorAll("span.line"),
-        translateY: ["100%", 0],
-        easing: "easeOutSine",
-        duration: 800,
-        delay: (_, i) => i * 100,
-      })
+    useEffect(() => {
+      if (inView) {
+        // Text line animation
+        anime({
+          targets: textRef.current.querySelectorAll("span.line"),
+          translateY: ["100%", 0],
+          easing: "easeOutSine",
+          duration: 800,
+          delay: (_, i) => i * 100,
+        })
+      }
+    }, [inView, textRef])
+
+    switch (as) {
+      case "p":
+        return (
+          <p
+            className={`${className} ${col ? "col-" + col : ""}`}
+            ref={setRefs}
+          >
+            {splitAndAnime && inner.current}
+            {!splitAndAnime && children}
+          </p>
+        )
+      case "h2":
+        return (
+          <h2
+            className={`${className} ${col ? "col-" + col : ""}`}
+            ref={setRefs}
+          >
+            {splitAndAnime && inner.current}
+            {!splitAndAnime && children}
+          </h2>
+        )
+      default:
+        return (
+          <p
+            className={`${className} ${col ? "col-" + col : ""}`}
+            ref={setRefs}
+          >
+            {splitAndAnime && inner.current}
+            {!splitAndAnime && children}
+          </p>
+        )
     }
-  }, [inView, textRef])
-
-  switch (as) {
-    case "p":
-      return (
-        <p className={`${className} ${col ? "col-" + col : ""}`} ref={setRefs}>
-          {splitAndAnime && inner}
-          {!splitAndAnime && children}
-        </p>
-      )
-    case "h2":
-      return (
-        <h2 className={`${className} ${col ? "col-" + col : ""}`} ref={setRefs}>
-          {splitAndAnime && inner}
-          {!splitAndAnime && children}
-        </h2>
-      )
-    default:
-      return (
-        <p className={`${className} ${col ? "col-" + col : ""}`} ref={setRefs}>
-          {splitAndAnime && inner}
-          {!splitAndAnime && children}
-        </p>
-      )
   }
-}
+)
 
 export default Text
--- src/components/text/line.js
+++ src/components/text/line.js
@@ -1,12 +1,12 @@
 import React from "react"
 import cn from "classname"
 
-const Line = ({ children, className }) => {
+const Line = React.memo(({ children, className }) => {
   return (
     <span className={cn("hide", className)}>
       <span className="line">{children}</span>
     </span>
   )
-}
+})
 
 export default Line
