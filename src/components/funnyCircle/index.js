c9c6ef51f3c8341e25c4c650413ad76532adc0f4
Armand Sallé <armand.salle@gmail.com>
2020-09-30 15:44:52 +0200

    memo funny circle

--- src/components/funnyCircle/index.js
+++ src/components/funnyCircle/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 
-const FunnyCircle = () => {
+const FunnyCircle = React.memo(() => {
   return (
     <div className="funny-circle">
       <div className="turn"></div>
     </div>
   )
-}
+})
 
 export default FunnyCircle
